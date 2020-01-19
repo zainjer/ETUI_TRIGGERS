dd5726ed029a4ac8ef4f3a88d777bd2fa7601366
zainjer <arainareeb@gmail.com>
2020-01-19 12:50:20 +0500

    Add project files.

---
+++ ETUI_TRIGGERS/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ETUI_TRIGGERS
+{
+    public partial class CreateNewTrigger : Form
+    {
+        public CreateNewTrigger()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateTrigger_Click(object sender, EventArgs e)
+        {
+            Trigger obj = new Trigger();
+
+            int positionX, positionY;
+
+            positionX = int.Parse(txtbxXposition.Text);
+            positionY = int.Parse(txtbxYposition.Text);
+
+            
+   
+            obj.Show();
+            obj.Location = new Point(positionX, positionY);
+            obj.Size = new Size(positionX, positionY);
+        }
+    }
+}
